48bc7958d0a521a798a6dfde97aa05a5b69319f3
Jan Goličnik <jan@nejka.net>
2024-12-13 22:19:59 +0100

    emacs is pain

---
+++ CMakeFiles/bskiji.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bskiji.dir/main.c.obj"
+  "CMakeFiles/bskiji.dir/main.c.obj.d"
+  "bskiji.exe"
+  "bskiji.exe.manifest"
+  "bskiji.pdb"
+  "libbskiji.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bskiji.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/bskiji.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for bskiji.
+# This may be replaced when dependencies are built.
